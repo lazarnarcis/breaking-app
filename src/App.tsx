b818635744103d7a176d79c8e7654c99bc20870c
Lazar Narcis <lnarcis310@gmail.com>
2022-07-21 11:32:33 +0300

    code better

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import "./style.scss";
 import axios from "axios";
 
 export default function App() {
-    const [users, setUsers] = useState<any[]>([]);
-    const input = useRef<any>(null);
-    const [apiLink, setApiLink] = useState("https://www.breakingbadapi.com/api/characters/");
-    const [usersMap, setUsersMap] = useState<any[]>([]);
-    const [text, setText] = useState<string>("Sort Users");
+    const [users, setUsers] = React.useState<any[]>([]);
+    const input = React.useRef<any>(null);
+    const [apiLink, setApiLink] = React.useState("https://www.breakingbadapi.com/api/characters/");
+    const [usersMap, setUsersMap] = React.useState<any[]>([]);
+    const [text, setText] = React.useState<string>("Sort Users");
 
-    useEffect(() => {
-        axios.get(apiLink)
-            .then(data => {
-                setUsers(data.data);
-            });
+    React.useEffect(() => {
+        axios.get(apiLink).then(data => setUsers(data.data));
     }, [apiLink]);
 
-    useEffect(() => {
-        console.log(users);
+    React.useEffect(() => {
         setUsersMap(
             users.map((user, key) => {
                 return <div key={key} className="card" style={{ width: "18rem" }}>
@@ -33,42 +29,26 @@ export default function App() {
 
     const onChangeHandler = () => {
         let inputValue = input.current.value;
-        setApiLink(`https://www.breakingbadapi.com/api/characters/?name=` + inputValue);
+        setApiLink(`https://www.breakingbadapi.com/api/characters/?name=${inputValue}`);
     }
 
     const sortCharacterAscending = () => {
-        setUsers(users => {
-            const usersToSort = [...users];
-            usersToSort.sort((a, b) => a.name.localeCompare(b.name));
-            return usersToSort;
-        });
+        setUsers(users => [...users].sort((a, b) => a.name.localeCompare(b.name)));
         setText("Character name ascending");
     }
 
     const sortCharacterDescending = () => {
-        setUsers(users => {
-            const usersToSort = [...users];
-            usersToSort.sort((a, b) => b.name.localeCompare(a.name));
-            return usersToSort;
-        });
+        setUsers(users => [...users].sort((a, b) => b.name.localeCompare(a.name)));
         setText("Character name descending");
     }
 
     const sortActorsAscending = () => {
-        setUsers(users => {
-            const usersToSort = [...users];
-            usersToSort.sort((a, b) => a.portrayed.localeCompare(b.portrayed));
-            return usersToSort;
-        });
+        setUsers(users => [...users].sort((a, b) => a.portrayed.localeCompare(b.portrayed)));
         setText("Actor name ascending");
     }
 
     const sortActorsDescending = () => {
-        setUsers(users => {
-            const usersToSort = [...users];
-            usersToSort.sort((a, b) => b.portrayed.localeCompare(a.portrayed));
-            return usersToSort;
-        });
+        setUsers(users => [...users].sort((a, b) => b.portrayed.localeCompare(a.portrayed)));
         setText("Actor name descending");
     }
 
@@ -77,8 +57,8 @@ export default function App() {
             <h1>Breaking Bad App</h1>
             <div className="input-element">
                 <div className="input-group mb-3">
-                    <input type="text" placeholder="Text field" className="form-control" aria-label="Text input with dropdown button" ref={input} onChange={onChangeHandler} />
-                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{ text }</button>
+                    <input type="text" placeholder="Name..." className="form-control" aria-label="Text input with dropdown button" ref={input} onChange={onChangeHandler} />
+                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{text}</button>
                     <ul className="dropdown-menu dropdown-menu-end">
                         <li><a className="dropdown-item" href="/#" onClick={sortCharacterAscending}>Character name ascending</a></li>
                         <li><a className="dropdown-item" href="/#" onClick={sortCharacterDescending}>Character name descending</a></li>
@@ -88,9 +68,7 @@ export default function App() {
                     </ul>
                 </div>
             </div>
-            <div className="cards">
-                { usersMap }
-            </div>
+            <div className="cards">{usersMap}</div>
         </div>
     );
 }
